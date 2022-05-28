3d7397def91c24595413239e2198e9afac8ffdb2
jovanaviloti <106406098+jovanaviloti@users.noreply.github.com>
2022-05-28 14:08:42 +0200

    Add files via upload

---
+++ Projekat/javascript.js
@@ -0,0 +1,20 @@
+import { dinos } from "./niz.js";
+const body = document.body;
+ const form = document.querySelector('form');
+ const ispis=document.querySelector('#btn');
+
+ form.addEventListener('submit', element => {
+    dinos.forEach(element  => {
+        let pKupac= document.createElement('pKupac')
+        pKupac = input.value.trim()
+        let pDinosaurus= document.createElement('pDinosaurus');
+        pDinosaurus = element.pDinosaurus
+
+        let pNapomena = document.createElement('pNapomena')
+        pNapomena= input.value.trim()
+
+      
+
+  })
+})
+    
